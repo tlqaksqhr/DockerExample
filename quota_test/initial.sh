dbbfd86fa94b4211287f55f0ebb0488b6acb9b34
semtax <tlqaksqhr@naver.com>
2019-01-03 13:18:07 +0900

    quota_test case 1 added...

---
+++ quota_test/initial.sh
@@ -0,0 +1,6 @@
+dd if=/dev/zero of=scratch.ext4 bs=1M count=128
+mkfs.ext4 -F scratch.ext4
+mkdir tmpfolder
+sudo mount scratch.ext4 tmpfolder -o loop
+sudo cp ./overflow_write.py tmpfolder/overflow_write.py
+docker run --rm -v "$(pwd)"/tmpfolder:/home/app -it quota_test /bin/bash
---
+++ quota_test/overflow_write.py
@@ -0,0 +1,6 @@
+
+
+f = open("./test.txt","w")
+size = 1024*1024*1024
+f.write("A"*size)
+f.close()
