3820b1c714342b8f60cba220c91495bb8c451ade
semtax <tlqaksqhr@naver.com>
2019-01-04 15:53:19 +0900

    tmpfs_quota_logic added...

---
+++ quota_logic/quota_scratch.py
@@ -0,0 +1,9 @@
+import subprocess
+from pathlib import Path
+import os
+
+def create_scratch_filesystem(scratch_dir,size):
+    subprocess.run(['sudo','mount','-t','tmpfs' '-o' f'size={size}M' 'tmpfs' f'{scratch_dir}'])
+
+def destroy_scratch_filesystem(scratch_dir):
+    subprocess.run(['sudo','umount',f'{scratch_dir}'])
